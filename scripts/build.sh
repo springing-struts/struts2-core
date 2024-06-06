91586a825e870b9ed0f339a6550165e4cba49099
iwauo tajima <iwauo.tajima@gmail.com>
2024-06-07 03:06:24 +0900

    manage gradle binary with asdf

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,15 +4,14 @@ SCRIPT_DIR=$(realpath $(dirname $BASH_SOURCE[0]))
 MODULE_ROOT_DIR=$(realpath $SCRIPT_DIR/../)
 PROJECT_ROOT_DIR=$(realpath $MODULE_ROOT_DIR/../)
 
-GRADLE=./gradlew
-
 main() {
   build
 }
 
 build() {
   (cd $MODULE_ROOT_DIR
-    $GRADLE publish
+    gradle publish
   )
 }
+
 main
